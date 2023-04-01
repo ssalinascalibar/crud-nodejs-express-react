aed3dbdc8c039992f7129da7b3cd490cefffeb43
ssalinascalibar <ssalinascalibar@gmail.com>
2023-03-31 21:52:58 -0300

    mod urls endpoint app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   console.log(currentUser); 
 
   const getUsers = async () => {
-    const endPoint = "http://localhost:8081/api";
+    const endPoint = "https://crud-nodejs-express-react-server.vercel.app/api";
     const response = await fetch(endPoint);
     const data = await response.json();
 
