a6c5430b1920bd7d455e1f4657eb4f2b6d525f8b
ssalinascalibar <ssalinascalibar@gmail.com>
2023-04-01 22:09:21 -0300

    mod app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -88,7 +88,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault(); // para que no se recargue la pagina al momento de procesar el formulario
     // fetch("http://localhost:8081/api", {
-    fetch("https://crud-nodejs-express-react-server.vercel.app/api", {
+    fetch("https://crud-nodejs-express-react.vercel.app/api", {
       method: "POST",
       body: JSON.stringify(
         newUser
@@ -109,7 +109,7 @@ function App() {
   const deleteUser = async (id) => {
     alert("Seguro que quiere eliminar a este usuario?");
     console.log(id);
-    await fetch(`http://localhost:8081/api/${id}`, {
+    await fetch(`https://crud-nodejs-express-react.vercel.app/api/${id}`, {
       method: "DELETE",
     }).then((response) => {
       if (response.status === 200) {
