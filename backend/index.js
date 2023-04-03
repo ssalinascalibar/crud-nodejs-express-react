c3b08165698a443fa55a2ffe24487ea0a703fd5a
ssalinascalibar <ssalinascalibar@gmail.com>
2023-04-03 10:48:36 -0400

    mod index js

--- backend/index.js
+++ backend/index.js
@@ -33,9 +33,9 @@ app.post("/api", (req, res) => {
 
     users.push(req.body);
 
-    fs.writeFile('./users.json', JSON.stringify([...users, req.body]), (err) => {
-        if (err) console.log('Error writing file:', err);
-    })
+    // fs.writeFile('./users.json', JSON.stringify([...users, req.body]), (err) => {
+    //     if (err) console.log('Error writing file:', err);
+    // })
     console.log("usuarios actuales" + users)
 });
 
