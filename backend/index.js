7a68276971907255b4446d1c2556fad0711b8d49
ssalinascalibar <ssalinascalibar@gmail.com>
2023-04-04 11:54:06 -0400

    mod index js

--- backend/index.js
+++ backend/index.js
@@ -70,16 +70,17 @@ app.put("/api/:id", (req, res) => {
     console.log('este es el nuevo usuario editado')
     res.send("api - Usuario editado - PUT");
 
+    
+      
+    fs.writeFile('./users.json', JSON.stringify([...users, req.body]), (err) => {
+        if (err) console.log('Error writing file:', err);
+    })
     const index = users.indexOf(
         users.find((user) => user.id === req.params.id.toString())
         );
         console.log("variable index = posición en array " + index)
         console.log("variable req.params = id del usuario editado " + req.params.id)
       users.splice(index, 1);
-      
-    fs.writeFile('./users.json', JSON.stringify([...users, req.body]), (err) => {
-        if (err) console.log('Error writing file:', err);
-    })
     console.log("usuarios actuales" + users)
 });
 
